66bca32176b4c89695542dc9cfb278cba0e85bce
zakir-ecl <zakir-ecl@experts.com.bd>
2020-11-09 14:19:29 +0600

    Charts and Image Search

---
+++ src/components/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Menu } from "./Menu";
+import { Home } from "./Home";
+import { Charts } from "./Charts";
+import { Search } from "./Search";
+import { Error } from "./Error";
+
+export const App = () => {
+  return (
+    <>
+      <Menu />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Charts" component={Charts} />
+        <Route exact path="/Search" component={Search} />
+        <Route component={Error} />
+      </Switch>
+    </>
+  );
+};
