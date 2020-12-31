778a177627c4d1d71685ad880dd6d15ea7b425c3
zakir-ecl <73765950+zakir-ecl@users.noreply.github.com>
2020-12-31 09:40:04 +0600

    Update Error.tsx

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Error=()=>{
     return(
         <>
-        <h1>This is Error Page</h1>
+        <h1>This is Error Page Go to Home page</h1>
         </>
     );
 }
